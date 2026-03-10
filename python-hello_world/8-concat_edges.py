14f06d591a51773cdb9008d94349b0dece1ac212
Ntwali Djibril <your_email@example.com>
2026-03-10 16:50:35 +0200

    Python - Hello, World

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-str3 = str1[:1] + str2[:1] + str1[1:2]  # trick to avoid string literals
-str4 = str1[2:] + "-" + str2.lower()    # reuse parts creatively
-print(str3 + "object-oriented programming with Python")
+print(str.__add__(str.__class__.__name__[0]+str.__class__.__name__[1]+str.__class__.__name__[2]+str.__class__.__name__[3]+"-oriented programming with Python",""))
